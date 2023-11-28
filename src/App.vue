156dce6af1769b8e03f3450d716fe7ef1eae3738
Alessio-Tosoni <alessiotosoni988@gmail.com>
2023-11-28 16:58:38 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import appheader from './components/appheader.vue'
+// import AppJumbo from './components/AppJumbo.vue'
+// import AppProdotti from './components/AppProdotti.vue'
+// import AppFooter from './components/AppFooter.vue'
+
+export default {
+	components: {
+		appheader,
+		// AppJumbo,
+		// AppProdotti,
+		// AppFooter,
+	}
+}
+</script>
+
+<template>
+	<AppHeader />
+	 <!-- <main> -->
+		<!--<AppJumbo /> -->
+		<!-- <AppProdotti />  -->
+	<!-- </main>  -->
+	<!-- <AppFooter /> -->
+</template>
+
+<style scoped></style>
---
+++ src/components/appheader.vue
@@ -0,0 +1,29 @@
+<script>
+    export default {
+        name: "appheader",
+        data() {
+            return {
+                menuLinks: [
+                    "CHARACTERS", "COMICS", "MOVIES", "TV", "GAMES", "COLLECTIBLES", "VIDEOS", "FANS", "NEWS", "SHOP"
+                ]
+            }
+        },
+        mounted() {
+            console.log("Componente AppHeader caricato");
+        }
+    }
+</script>
+
+<template>
+    <header>
+        <nav>
+            <a v-for="link in menuLinks" :href="link.url">{{ link.text }}</a>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+    nav a {
+        margin: 0 1rem;
+    }
+</style>
